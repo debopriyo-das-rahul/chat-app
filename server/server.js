f6a5cea17cdb3cfb0da433fca2606d2fe9c56423
debopriyo-das-rahul <rahuldas4740@gmail.com>
2026-04-12 18:53:14 +0600

    Added message controller,  user route, message route, cloudinary for sending images

--- server/server.js
+++ server/server.js
@@ -3,17 +3,46 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+
+// initialize socket.io server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// store online user
+export const userSocketMap = {}; //{userid: socketId}
+
+// socket.io connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected", userId);
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
+//route setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
-
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
